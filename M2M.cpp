966cb3634d928ff00521cc3dc0fec886dfe26625
Преподобный Ален <alienufo@inbox.ru>
2020-09-16 17:43:04 +0300

    first commit

---
+++ M2M.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alien on 16.09.2020.
+//
+
+#include "M2M.hpp"
---
+++ M2M.hpp
@@ -0,0 +1,14 @@
+//
+// Created by alien on 16.09.2020.
+//
+
+#ifndef APOSTOL_PLUGME_M2M_HPP
+#define APOSTOL_PLUGME_M2M_HPP
+
+
+class M2M {
+
+};
+
+
+#endif //APOSTOL_PLUGME_M2M_HPP
